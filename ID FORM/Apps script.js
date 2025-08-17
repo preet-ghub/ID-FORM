754d9917b27e9d7737435a78e29ed95ae86ddfa2
Preet Raj <praj63606@gmail.com>
2025-08-18 02:38:26 +0530

    Update Apps script.js

--- ID FORM/Apps script.js
+++ ID FORM/Apps script.js
@@ -1,36 +1,51 @@
 function doPost(e) {
-    try {
-      var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-      var data = e.parameter;
-  
-      var fileUrl = "";
-      if (e.parameters.photo && e.parameters.photo.length > 0) {
-        var blob = Utilities.newBlob(Utilities.base64Decode(e.parameters.photo), "image/jpeg", "photo.jpg");
-        var folder = DriveApp.getFolderById("1gcp5utWeBvRtgHakaIOquZMOGK2bpJNe"); // create a folder in Google Drive and paste ID here
-        var file = folder.createFile(blob);
-        fileUrl = file.getUrl();
-      }
-  
-      sheet.appendRow([
-        new Date(),
-        data.name,
-        data.fatherName,
-        data.motherName,
-        data.phone,
-        data.guardianPhone,
-        data.admissionNo,
-        data.rollNo,
-        data.section,
-        data.email,
-        data.dob,
-        data.address,
-        fileUrl
-      ]);
-  
-      return ContentService.createTextOutput("Success").setMimeType(ContentService.MimeType.TEXT);
-    } catch (err) {
-      return ContentService.createTextOutput("Error: " + err).setMimeType(ContentService.MimeType.TEXT);
+  try {
+    var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+    var folder = DriveApp.getFolderById("1nlCWTz-c6V1vh3czfq55gOwG7lgoQGrZ");
+
+    var name = e.parameter.name;
+    var fatherName = e.parameter.fatherName;
+    var motherName = e.parameter.motherName;
+    var phone = e.parameter.phone;
+    var guardianPhone = e.parameter.guardianPhone;
+    var admissionNo = e.parameter.admissionNo;
+    var rollNo = e.parameter.rollNo;
+    var section = e.parameter.section;
+    var email = e.parameter.email;
+    var dob = e.parameter.dob;
+    var address = e.parameter.address;
+
+    var fileUrl = "";
+
+    if (e.parameter.photoLink && e.parameter.photoLink.length > 0) {
+      var blob = Utilities.newBlob(Utilities.base64Decode(e.parameter.photoLink), "image/jpeg", name + "_photo.jpg");
+      var file = folder.createFile(blob);
+
+      
+      fileUrl = '=HYPERLINK("' + file.getUrl() + '","' + name + '")';
     }
+
+    sheet.appendRow([
+      new Date(),
+      name,
+      fatherName,
+      motherName,
+      phone,
+      guardianPhone,
+      admissionNo,
+      rollNo,
+      section,
+      email,
+      dob,
+      address,
+      fileUrl
+    ]);
+
+    return ContentService.createTextOutput("Success")
+      .setMimeType(ContentService.MimeType.TEXT);
+
+  } catch (err) {
+    return ContentService.createTextOutput("Error: " + err)
+      .setMimeType(ContentService.MimeType.TEXT);
   }
-  
-  
+}
